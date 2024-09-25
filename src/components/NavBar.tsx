f27ec33d19fa04781d02f7535668375a4758c836
TheGhossst <nandukrishnapkm97@gmail.com>
2024-09-25 19:42:13 +0530

    add: lucide-react and react-icons dependencies, update NavBar component with new UI, and add CSS styles for navigation bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,49 @@
+import { useState } from 'react';
+import logo from '../assets/images/logo.png';
+import github from '../assets/images/github.png';
+import { Menu, X } from 'lucide-react';
 
+export function NavBar() {
+  const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+  
+  const toggleNavbar = () => {
+    setMobileDrawerOpen(!mobileDrawerOpen);
+  };
 
-export function NavBar(){
-    return (
-        <>
-        </>
-    )
+  return (
+    <nav className = "navbar">
+      <div className = "container">
+        <div className = "nav-content">
+          <div className = "logo-container">
+            <img 
+                className = "logo" 
+                src = { logo } 
+                alt = "Logo" 
+            />
+            <span className = "brand-name">TuneTagger</span>
+          </div>
+          <div className = {`nav-links ${mobileDrawerOpen ? 'active' : ''}`}>
+            <a 
+                href = "https://github.com/TheGhossst" 
+                target = "_blank" 
+                className = "nav-icon-link">
+                <img 
+                    src={ github } 
+                    alt="GitHub Icon" 
+                    className="github-icon" 
+                />
+            </a>
+          </div>
+          <div className = "mobile-menu">
+            <button 
+                onClick = { toggleNavbar } 
+                aria-expanded = { mobileDrawerOpen } 
+                aria-label = "Toggle navigation"
+            >{mobileDrawerOpen ? <X /> : <Menu />}
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
