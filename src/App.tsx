d429ec06e34891792bcf86f4fcf0d98ef47ba07e
TheGhossst <nandukrishnapkm97@gmail.com>
2024-09-25 20:13:05 +0530

    add: Hero component to App.tsx and update index.css with new styles for Hero component and container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { NavBar } from './components/NavBar'
+import { Hero } from './components/Hero'
 
 export function App(){
   return (
     <>
       <NavBar />
       <div className = 'container'>
-        
+        <Hero />
       </div>
     </>
   )
---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+
+export function Hero(){
+    return (
+        <div className = "hero-container">
+            <img 
+                src = ""
+                alt = "Hero Image"
+                className = "hero-image"
+            />
+            <h1>Welcome to TuneTagger</h1>
+            <h4>Place where tagging music becomes easy</h4>
+            <button>Start Tagging</button>
+        </div>
+    )
+}
