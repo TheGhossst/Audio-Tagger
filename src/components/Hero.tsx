2ee9b6e67439590f3168916e136bc93a78448e41
TheGhossst <nandukrishnapkm97@gmail.com>
2024-10-06 00:44:32 +0530

    Comment out hero image import and img tag in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,13 @@
-import heroimage from '../assets/images/heroimage.png'
+//import heroimage from '../assets/images/heroimage.png'
 
 export function Hero(){
     return (
         <div className = "hero-container">
-            <img 
+            {/*<img 
                 src = { heroimage }
                 alt = "Hero Image"
                 className = "hero-image"
-            />
+            />*/}
             <h1>Welcome to TuneTagger</h1>
             <h4>Place where tagging music becomes easy</h4>
             <button>Start Tagging</button>
