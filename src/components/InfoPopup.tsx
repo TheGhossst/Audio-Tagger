70cbe398630deb79acd24eeebacdb06d625d8ef8
TheGhossst <nandukrishnapkm97@gmail.com>
2024-10-06 18:09:41 +0530

    add: close button to InfoPopup component and style it in index.css

--- src/components/InfoPopup.tsx
+++ src/components/InfoPopup.tsx
@@ -1,9 +1,16 @@
+import { X } from 'lucide-react';
 
 export function InfoPopup(){
   return (
     <div className = "popup">
-      <div className="popup-container">
-        
+      <div className= "popup-container">
+        <button 
+          onClick={() => alert("Click1")}
+          className= "popup-btn"
+          aria-label="Close"
+        >
+          <X size={24} />
+        </button>
       </div>
     </div>
   );
