68721823ea9ce6f63543c9e7a85407c5793c5a4a
TheGhossst <nandukrishnapkm97@gmail.com>
2024-10-06 18:12:55 +0530

    add: isOpen and onClose props, and render conditionally based on isOpen

--- src/components/InfoPopup.tsx
+++ src/components/InfoPopup.tsx
@@ -1,17 +1,23 @@
 import { X } from 'lucide-react';
 
-export function InfoPopup(){
-  return (
-    <div className = "popup">
-      <div className= "popup-container">
-        <button 
-          onClick={() => alert("Click1")}
-          className= "popup-btn"
-          aria-label="Close"
-        >
-          <X size={24} />
-        </button>
-      </div>
-    </div>
-  );
+type InfoPopupProps = {
+    isOpen: boolean;
+    onClose: () => void;
+}
+export function InfoPopup({ isOpen, onClose }: InfoPopupProps) {
+    if (!isOpen) return null;
+    return (
+        <div className = "popup">
+        <div className= "popup-container">
+            <button 
+            /*onClick={() => alert("Click1")}*/
+            onClick={ onClose }
+            className= "popup-btn"
+            aria-label="Close"
+            >
+            <X size={24} />
+            </button>
+        </div>
+        </div>
+    );
 };
